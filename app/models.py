3231d664a1cf69d19bb896ef0462fb8f1f041da5
kazeem jamiu <kazeemj565@gmail.com>
2025-10-23 00:56:59 +0100

    factor

--- app/models.py
+++ app/models.py
@@ -30,6 +30,7 @@ class AnalyzedString(Base):
     id = Column(String, primary_key=True, index=True)  # SHA256 hash used as ID
     value = Column(String, unique=True, nullable=False)
     length = Column(Integer, nullable=False)
+    sha256_hash = Column(String, nullable=False, unique=True, index=True)
     is_palindrome = Column(Boolean, nullable=False)
     unique_characters = Column(Integer, nullable=False)
     word_count = Column(Integer, nullable=False)
